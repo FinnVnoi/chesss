a0aaf5b07525ea1ade6952d50f1983b378a2ac51
FinnVnoi <134766874+FinnVnoi@users.noreply.github.com>
2024-02-13 18:41:58 +0700

    điều chỉnh

--- script.js
+++ script.js
@@ -207,7 +207,7 @@ function playRandomMusic() {
 
 // Xác định mốc thời gian mục tiêu (25-9-2023 23:59:59)
 const tdate = new Date("2023-11-31T23:59:59").getTime();
-const rdate = new Date("2024-01-30T23:59:59").getTime();
+const rdate = new Date("2024-03-01T23:59:59").getTime();
 // Lấy thời gian hiện tại
 const cdate = new Date().getTime();
 // Tính khoảng thời gian còn lại đến mốc thời gian mục tiêu
@@ -301,16 +301,16 @@ function removeNoteFromCards(cardList) {
     //addCustomCard();
     audio1.src = 'music.mp3'
     audio1.play();
-  addNoteToCards([54, 50, 46], "BUFF");
-  addNoteToCards([36], "NERF");
-  addNoteToCards([22], "ADJUST");
-  addNoteToCards([75,76,77,78,79,80,81], "REWRITE");
+  addNoteToCards([38], "BUFF: Lượt bạn -> Bất kì");
+  addNoteToCards([22,59,43], "ADJUST");
+  addNoteToCards([1], "NERF: Hủy -> Vô hiệu");
+  addNoteToCards([58], "NERF: All slưu -> 2 quân");
   } else {audio1.play()}
   if (rremaining < 0) {
     // Sử dụng hàm để xóa ghi chú từ các lá bài trong mảng newcard
     removeNoteFromCards([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81]);
-    audio1.src = 'tetmusic.mp3'
-    audio1.play();
+    //audio1.src = 'tetmusic.mp3'
+    //audio1.play();
     //for (let z = 69; z <= 72; z++) { removeCardById(z); }
   }
 // Chọn lá bài
